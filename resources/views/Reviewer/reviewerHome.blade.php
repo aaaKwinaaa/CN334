25b083d8b73f2cc414abe75f5368c2fbd704c358
Sahatsawat Kewjaroen <60878851+sahatsawat-kew@users.noreply.github.com>
2021-02-25 18:21:30 +0700

    add content in Reviewer Home
    view->Reviewer->reviewerHome.blade.php

--- resources/views/Reviewer/reviewerHome.blade.php
+++ resources/views/Reviewer/reviewerHome.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                {{-- <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,9 +16,33 @@
 
                     {{ __('You are logged in!') }}
                     You are Reviewer.
-                </div>
-            </div>
-        </div>
+                </div> --}}
+
+                <section class="ftco-section">
+                    <div class="container">
+                        <div class="row justify-content-center mb-5 pb-3">
+                            <div class="col-md-7 heading-section ftco-animate text-center fadeInUp ftco-animated">
+                                <h2 class="mb-4 mt-4">Restaurant in This Web</h2>
+                                <p>All restaurant in this web is waiting you to review.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-wrap">
+                        <div class="row no-gutters d-flex">
+                            <div class="col-lg-4 d-flex ftco-animate fadeInUp ftco-animated">
+                                <div class="services-wrap d-flex">
+                                    <a href="#" class="img" style="background-image: url(images/pizza-1.jpg);"></a>
+                                    <div class="text p-4">
+                                        <h3>Italian Pizza</h3>
+                                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            {{-- </div>
+        </div> --}}
     </div>
 </div>
 @endsection
